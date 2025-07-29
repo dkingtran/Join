1612e689130b6ea06d9a0439e0bf5809449d6122
Andreas Kissner <andikiss500@gmail.com>
2025-07-29 17:12:48 +0200

    Add class for subtask-ouptu

--- scripts/add-task-templates.js
+++ scripts/add-task-templates.js
@@ -4,7 +4,7 @@ function getSubtaskTemplate(text){
     <div class="subtask-entry font-bundle">${text}</div>
     <div class="icon-edit-subtask-box">
         <img class="icon-task" src="assets/img/icons/add_task/Property1=edit.svg" alt="Edit Icon">
-        <img class="icon-task" src="assets/img/icons/add_task/delete.svg" alt="Cancel Icon">
+        <img class="icon-task" src="assets/img/icons/add_task/delete.svg" alt="Cancel Icon" onclick="cancelSubtaskInput()">
     </div>
 </div>
  `;
