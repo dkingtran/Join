42e42d4e3e36cec9b9076a641bacbea194ebcabe
Andreas Kissner <andikiss500@gmail.com>
2025-07-29 16:16:52 +0200

    Add in getSubtaskTemplate 2 new icons

--- scripts/add-task-templates.js
+++ scripts/add-task-templates.js
@@ -1,6 +1,11 @@
 function getSubtaskTemplate(text){
     return  `
-    
-  <div class="subtask-entry font-bundle">${text}</div>
+   <div class="subtask-text-box">
+    <div class="subtask-entry font-bundle">${text}</div>
+    <div class="icon-edit-subtask-box">
+        <img class="icon-task" src="assets/img/icons/add_task/Property1=edit.svg" alt="Edit Icon">
+        <img class="icon-task" src="assets/img/icons/add_task/delete.svg" alt="Cancel Icon">
+    </div>
+</div>
  `;
 }
