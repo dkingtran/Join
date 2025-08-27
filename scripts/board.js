2eebb1bfe029279a0151d528f15f00c0b39bfce2
mtresor01 <kmarius2@yahoo.fr>
2025-08-27 13:03:33 +0200

    board js update

--- scripts/board.js
+++ scripts/board.js
@@ -1,14 +1,43 @@
 let tasks = [];
-
 let displayedTasks = [];
+let contacts = [];
 
 async function init() {
+    await updateTasksArrays();
+    await getContactsArray();
+}
+
+async function updateTasksArrays() {
     await getTasksArray();
+    displayedTasks = tasks;
 }
 
 async function getTasksArray() {
-    let contactObjects = await loadData("/tasks/");
+    tasks = [];
+    let taskObjects = await loadData("/tasks/");
+    if (!taskObjects) {
+        console.warn("no tasks available.");
+        return;
+    }
+    Object.keys(taskObjects).forEach(key => {
+        tasks.push(taskObjects[key]);
+    });
+}
+
+/**
+ * @async
+ * @function
+ * 
+ * Asynchronously loads contact objects from the "/contacts/" endpoint and populates the global `contacts` array.
+ */
+async function getContactsArray() {
+    contacts = [];
+    let contactObjects = await loadData("/contacts/");
+    if (!contactObjects) {
+        console.warn("no contacts available.");
+        return;
+    }
     Object.keys(contactObjects).forEach(key => {
-        tasks.push(contactObjects[key]);
+        contacts.push(contactObjects[key]);
     });
 }
